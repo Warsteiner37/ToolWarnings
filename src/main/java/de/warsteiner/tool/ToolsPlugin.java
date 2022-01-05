7014e75fe575118fe4af656a2169e8c164e385e7
Warsteiner37 <66200072+Warsteiner37@users.noreply.github.com>
2022-01-05 14:18:19 +0100

    Initial commit

---
+++ src/main/java/de/warsteiner/tool/ToolsPlugin.java
@@ -0,0 +1,62 @@
+package de.warsteiner.tool;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import de.warsteiner.tool.listener.PlayerBlockBreak;
+import de.warsteiner.tool.utils.YamlConfigFile;
+ 
+
+public class ToolsPlugin extends JavaPlugin {
+
+	private static ToolsPlugin plugin;
+
+	private YamlConfigFile config;
+ 
+	@Override
+	public void onEnable() {
+
+		plugin = this;
+ 
+			createFolders();
+
+			setupConfigs();
+
+		   Bukkit.getPluginManager().registerEvents(new PlayerBlockBreak(this.config.getConfig()), this);
+ 
+			this.getLogger().info("§4§lToolWarings §asuccessfull loaded!");
+ 
+	}
+ 
+	public static ToolsPlugin getPlugin() {
+		return plugin;
+	}
+ 
+	private void createFolders() {
+		if (!getDataFolder().exists()) {
+			getDataFolder().mkdir();
+		}
+	}
+
+	public void setupConfigs() {
+		File file_config = new File(getDataFolder() + File.separator, "Config.yml");
+
+		config = new YamlConfigFile(getPlugin(), file_config);
+
+		try {
+			config.load();
+		} catch (IOException e) {
+			this.getLogger().warning("§4§lFailed to create Config Files");
+			e.printStackTrace();
+		}
+	}
+ 
+	public YamlConfigFile getMainConfig() {
+		return config;
+	}
+}
---
+++ src/main/java/de/warsteiner/tool/listener/PlayerBlockBreak.java
@@ -0,0 +1,101 @@
+package de.warsteiner.tool.listener;
+
+import java.util.HashMap;
+import java.util.concurrent.CompletableFuture;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.inventory.ItemStack;
+ 
+import de.warsteiner.tool.ToolsPlugin;
+import net.md_5.bungee.api.ChatColor;
+
+public class PlayerBlockBreak implements Listener {
+	
+	private YamlConfiguration cfg;
+
+	private static final Pattern pattern = Pattern.compile("(?<!\\\\)(#[a-fA-F0-9]{6})");
+ 
+	
+	public PlayerBlockBreak(YamlConfiguration cfg) {
+		this.cfg = cfg;
+	}
+ 
+	@EventHandler(priority = EventPriority.HIGHEST)
+	public void onBreak(BlockBreakEvent event) {
+		Bukkit.getScheduler().runTaskAsynchronously(ToolsPlugin.getPlugin(), () -> {
+			try {
+				
+				Player player = event.getPlayer();
+				
+				if(event.isCancelled()) {
+					event.setCancelled(true);
+					return;
+				}
+				
+				if(player.getItemInHand() == null) {
+					return;
+				}
+		 
+				if(player.getItemInHand().getType() == null || player.getItemInHand().getType() == Material.AIR) {
+					return;
+				}
+				ItemStack i = player.getItemInHand();
+				
+				if(!cfg.getStringList("Items").contains(""+i.getType())) {
+					return;
+				}
+				
+				if(!cfg.getStringList("Worlds").contains(player.getWorld().getName())) {
+					return;
+				}
+				
+				short dua = i.getDurability();
+				short item = i.getType().getMaxDurability();
+				
+				int warning = cfg.getInt("WarnAt");
+				int rechnung = item - dua;
+				 
+				if(rechnung <= warning) { 
+				 
+					if(cfg.getBoolean("Enable_Title")) {
+						
+						String title1 = cfg.getString("Title_First").replaceAll("&", "§");
+						String title2 = cfg.getString("Title_Second").replaceAll("&", "§");
+						
+						player.sendTitle(toHex(title1), toHex(title2));
+					}
+					
+					if(cfg.getBoolean("Enable_Message")) {
+						
+						String m = cfg.getString("Message").replaceAll("&", "§");
+					 
+						player.sendMessage(toHex(m));
+					}
+					
+				}
+			} catch (Exception e) { 
+				e.printStackTrace();
+			}
+		});
+	}
+	 
+	public String toHex(String motd) {
+		Matcher matcher = pattern.matcher(motd);
+		while (matcher.find()) {
+			String color = motd.substring(matcher.start(), matcher.end());
+			motd = motd.replace(color, "" + ChatColor.of(color));
+		}
+
+		return motd;
+	}
+	
+}
